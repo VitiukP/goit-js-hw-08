e036d1dc76e2d0bc61edcc1ec4298b2e68523e28
VitiukP <107851187+VitiukP@users.noreply.github.com>
2023-07-26 23:14:36 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,4 +3,50 @@ import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+    })
+    .join('');
+}
+
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+
+  const isGalleryImageEl = event.target.classList.contains('gallery__image');
+  if (!isGalleryImageEl) {
+    return;
+  }
+
+  const largeImageUrl = event.target.dataset.source;
+  openModal(largeImageUrl);
+}
+
+
+function openModal(url) {
+  const instance = new SimpleLightbox(`
+    <img src="${url}" width="800" height="600">
+  `);
+
+  instance.show();
+}
+
