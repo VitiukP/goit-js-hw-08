945e44c97af7cf6330990ee757b2af68f8999b2f
VitiukP <107851187+VitiukP@users.noreply.github.com>
2023-07-27 13:22:06 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const inputEmail = form.elements.email; 
+const inputMessage = form.elements.message;
+const localStorageKey = 'feedback-form-state';
+let savedData = localStorage.getItem(localStorageKey);
+
+if (savedData) {
+    savedData = JSON.parse(savedData);
+    inputEmail.value = savedData.email;
+    inputMessage.value = savedData.message;
+}
+
+form.addEventListener('input', throttle(saveToLocalStorage, 500));
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log({
+        email: inputEmail.value,
+        message: inputMessage.value,
+    });
+
+    inputEmail.value = '';
+    inputMessage.value = '';
+    localStorage.removeItem(localStorageKey);
+});
+
+function saveToLocalStorage() {
+    const data = {
+        email: inputEmail.value,
+        message: inputMessage.value,
+    };
+
+    localStorage.setItem(localStorageKey, JSON.stringify(data));
+}
 
