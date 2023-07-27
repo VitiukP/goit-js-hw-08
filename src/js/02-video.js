f14fc37e70b8c39b07502d1e09821eb541a324dc
VitiukP <107851187+VitiukP@users.noreply.github.com>
2023-07-27 11:38:32 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,16 +3,16 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
-// Функция для сохранения текущего времени воспроизведения
+
 const saveCurrentTime = throttle(time => {
-  localStorage.setItem('videoplayer-current-time', time);
+    localStorage.setItem('videoplayer-current-time', time);
 }, 1000);
-// Отслеживание события timeupdate
+
 player.on('timeupdate', data => {
-  saveCurrentTime(data.seconds);
+    saveCurrentTime(data.seconds);
 });
-// Установка времени воспроизведения при перезагрузке страницы
+
 const savedTime = localStorage.getItem('videoplayer-current-time');
 if (savedTime) {
-  player.setCurrentTime(savedTime);
+    player.setCurrentTime(savedTime);
 }
