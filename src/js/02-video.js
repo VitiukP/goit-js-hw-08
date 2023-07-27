453a7bd97108c29c6177ae6289969da6029f1b94
VitiukP <107851187+VitiukP@users.noreply.github.com>
2023-07-27 11:24:32 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,25 +1,18 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const player = new Vimeo.Player(document.getElementById('vimeo-player'));
-
-const localStorageKey = 'videoplayer-current-time';
-
-// Функция для сохранения текущего времени воспроизведения в локальное хранилище
-const saveCurrentTime = throttle((currentTime) => {
-  localStorage.setItem(localStorageKey, currentTime);
-}, 1000); // Опция throttle - ограничение частоты вызовов до раз в 1000 мс (1 секунда)
-
-// Инициализация плеера и обработка события timeupdate для сохранения времени
-player.on('timeupdate', function (data) {
-  const currentTime = data.seconds;
-  saveCurrentTime(currentTime);
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+// Функция для сохранения текущего времени воспроизведения
+const saveCurrentTime = throttle(time => {
+  localStorage.setItem('videoplayer-current-time', time);
+}, 1000);
+// Отслеживание события timeupdate
+player.on('timeupdate', data => {
+  saveCurrentTime(data.seconds);
 });
-
-// Восстановление времени воспроизведения при перезагрузке страницы
-const storedTime = localStorage.getItem(localStorageKey);
-if (storedTime) {
-  player.setCurrentTime(storedTime).catch((error) => {
-    console.error('Failed to set player currentTime:', error);
-  });
+// Установка времени воспроизведения при перезагрузке страницы
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+  player.setCurrentTime(savedTime);
 }
